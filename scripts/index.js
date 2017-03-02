1368d5d705878b4fa573054146b44b13a1a9ecac
Magic <782633923@qq.com>
2017-03-02 16:40:05 +0800

    basic

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,9 @@
 /**
  * Created by Magic on 2017/3/2.
  */
+(function (angular) {
+    angular.module('bzModule')
+        .controller('bzController',["$scope",function ($scope) {
+            
+        }]);
+})(window.angular);
